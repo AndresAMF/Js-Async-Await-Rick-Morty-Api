73a61452cc4716972a56e98cfd91a15b6a4be88a
AndresAMF <andres.1991.munoz@gmail.com>
2022-06-29 19:54:57 -0400

    Se añaden getters

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,14 @@
 const cardContainer = document.querySelector(".card-container");
 
-
-
 const getCharacter = async (id) => {
   try {
     const response = await fetch(`https://rickandmortyapi.com/api/character/${id}/`);
     const data = await response.json();
-    const personaje = new Character(data);
-    CreateCard(personaje);
+    new Character(data);
   } catch (e) {
     console.log(e);
   }
 };
-
-
 
 const getCharacters = async (number) => {
   for (let i = 1; i <= number; i++) {
@@ -21,65 +16,41 @@ const getCharacters = async (number) => {
   }
 };
 
-
-
 getCharacters(20);
 
 
-
-const CreateCard = (personaje) => {
-
-  const card = document.createElement("div");
-  card.classList.add("card");
-
-  const cardImgContainer = document.createElement("div");
-  cardImgContainer.classList.add("img-container");
-
-  const cardImg = document.createElement("img");
-  cardImg.src = personaje.image;
-
-  cardImgContainer.appendChild(cardImg);
-
-  const cardName = document.createElement("p");
-  cardName.classList.add("name");
-  cardName.textContent = "Name: "+personaje.name;
-
-  const cardId = document.createElement("p");
-  cardId.classList.add("id");
-  cardId.textContent = "Id: "+personaje.id;
-
-  const cardStatus = document.createElement("p");
-  cardStatus.classList.add("status");
-  cardStatus.textContent = "Status: "+personaje.status;
-
-  const cardGender = document.createElement("p");
-  cardGender.classList.add("gender");
-  cardGender.textContent = "Gender: "+personaje.gender;
-
-  const cardSpecies = document.createElement("p");
-  cardSpecies.classList.add("species");
-  cardSpecies.textContent = "Species: "+personaje.species;
-
-  card.appendChild(cardImgContainer);
-  card.appendChild(cardName);
-  card.appendChild(cardId);
-  card.appendChild(cardStatus);
-  card.appendChild(cardGender);
-  card.appendChild(cardSpecies);
-
-  cardContainer.appendChild(card);
-};
-
-
-
-
 class Character {
-  constructor({ id, name, status, image, species, gender }) {
-    this.id = id;
-    this.name = name;
-    this.status = status;
-    this.image = image;
-    this.species = species;
-    this.gender = gender;
+  constructor({ name, image, species }) {
+    this._name = name;
+    this._image = image;
+    this._species = species;
+    this.show();
+  }
+
+  show(){
+    const card = document.createElement("div");
+    card.classList.add("card");
+  
+    const cardImgContainer = document.createElement("div");
+    cardImgContainer.classList.add("img-container");
+  
+    const cardImg = document.createElement("img");
+    cardImg.src = this._image;
+  
+    cardImgContainer.appendChild(cardImg);
+  
+    const cardName = document.createElement("p");
+    cardName.classList.add("name");
+    cardName.textContent = "Name: "+this._name;
+  
+    const cardSpecies = document.createElement("p");
+    cardSpecies.classList.add("species");
+    cardSpecies.textContent = "Species: "+this._species;
+  
+    card.appendChild(cardImgContainer);
+    card.appendChild(cardName);
+    card.appendChild(cardSpecies);
+  
+    cardContainer.appendChild(card);
   }
 }
